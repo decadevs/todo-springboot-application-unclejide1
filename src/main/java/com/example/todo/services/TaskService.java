57ea34db344c0fbdd9b9ce399013ee92d50b4d63
Oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-02-07 00:17:01 +0100

    add service folder that contains taskService class to implement business logic

---
+++ src/main/java/com/example/todo/services/TaskService.java
@@ -0,0 +1,24 @@
+package com.example.todo.services;
+
+import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task){
+        Task createdTask = null;
+
+        try {
+            createdTask = taskRepository.save(task);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return createdTask;
+    }
+}
